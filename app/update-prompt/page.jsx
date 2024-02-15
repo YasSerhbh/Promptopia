2c4c5ba3f830a6848bdd0a9894a472a967f4ac3a
YasSerHbh <hbhyasser@gmail.com>
2024-02-15 02:10:40 +0100

    Bug Fixed

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useEffect, useState} from 'react'
+import {useEffect, useState, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import Form from '@components/Form'
@@ -66,6 +66,7 @@ const EditPost = () => {
 
   return (
 
+    <Suspense fallback={<div>Loading...</div>}>
     <Form
         type='Edit'
         post={post}
@@ -73,6 +74,7 @@ const EditPost = () => {
         submitting={submitting}
         handleSubmit={editPrompt}
     />
+    </Suspense>
   )
 }
 
